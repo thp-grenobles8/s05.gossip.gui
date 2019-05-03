ddb8ebe6b41c4a2becd4838ed3066e4e8c12762d
gui3 <guillaume.3.7.13@gmail.com>
2019-05-03 18:10:03 +0200

    heroku user.remember_digest error

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    update_attribute('remember_digest', User.digest(remember_token))
   end
 
   def User.digest(string)
